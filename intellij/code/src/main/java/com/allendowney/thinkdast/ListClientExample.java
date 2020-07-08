5bc58a5139f617a53c3bd91263c930f50ded955c
weekbelt <vfrvfr4207@gmail.com>
2020-07-08 13:11:04 +0900

    ArrayList

--- intellij/code/src/main/java/com/allendowney/thinkdast/ListClientExample.java
+++ intellij/code/src/main/java/com/allendowney/thinkdast/ListClientExample.java
@@ -1,6 +1,6 @@
 package com.allendowney.thinkdast;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListClientExample {
@@ -9,7 +9,7 @@ public class ListClientExample {
 
 	@SuppressWarnings("rawtypes")
 	public ListClientExample() {
-		list = new LinkedList();
+		list = new ArrayList();
 	}
 
 	@SuppressWarnings("rawtypes")
--- intellij/code/src/main/java/com/allendowney/thinkdast/MyArrayList.java
+++ intellij/code/src/main/java/com/allendowney/thinkdast/MyArrayList.java
@@ -45,7 +45,14 @@ public class MyArrayList<T> implements List<T> {
 	@Override
 	public boolean add(T element) {
 		// TODO: FILL THIS IN!
-		return false;
+		if(size >= array.length) {
+			T[] bigger = (T[]) new Object[array.length * 2];
+			System.arraycopy(array, 0, bigger, 0, array.length);
+			array = bigger;
+		}
+		array[size] = element;
+		size++;
+		return true;
 	}
 
 	@Override
@@ -111,7 +118,12 @@ public class MyArrayList<T> implements List<T> {
 	@Override
 	public int indexOf(Object target) {
 		// TODO: FILL THIS IN!
-		return -1;
+        for (int i=0; i<size; i++) {
+        	if(equals(target, array[i])) {
+        		return i;
+			}
+		}
+        return -1;
 	}
 
 	/** Checks whether an element of the array is the target.
@@ -182,7 +194,12 @@ public class MyArrayList<T> implements List<T> {
 	@Override
 	public T remove(int index) {
 		// TODO: FILL THIS IN!
-		return null;
+		T element = get(index);
+		for(int i=index; i<size-1; i++) {
+			array[i] = array[i+1];
+		}
+		size--;
+		return element;
 	}
 
 	@Override
@@ -202,7 +219,9 @@ public class MyArrayList<T> implements List<T> {
 	@Override
 	public T set(int index, T element) {
 		// TODO: FILL THIS IN!
-		return null;
+		T old = get(index);
+		array[index] = element;
+		return old;
 	}
 
 	@Override
